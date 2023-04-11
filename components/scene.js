01e6ff489ae70cf4ba8c399b2b8a77a072a771b5
gabrielwebphy <gabriel.morgado@webphy.systems>
2023-04-11 15:06:31 -0300

    Segmentação automática no Javascript!

--- components/scene.js
+++ components/scene.js
@@ -2,7 +2,61 @@ import React, { useRef, useEffect } from 'react';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 //import marmore from '../textures/marmore.jpg'
 import * as THREE from 'three';
+import * as turf from '@turf/turf';
 
+function dividePolygon(poly, rows, cols) {
+    // Step 1: Find outer bounds of polygon
+    let min_x = Number.POSITIVE_INFINITY;
+    let min_y = Number.POSITIVE_INFINITY;
+    let max_x = Number.NEGATIVE_INFINITY;
+    let max_y = Number.NEGATIVE_INFINITY;
+    for (let i = 0; i < poly.length; i++) {
+        const p = poly[i];
+        min_x = Math.min(min_x, p.x);
+        min_y = Math.min(min_y, p.y);
+        max_x = Math.max(max_x, p.x);
+        max_y = Math.max(max_y, p.y);
+    }
+
+    // Step 2: Divide rectangle into grid
+    const cell_width = (max_x - min_x) / cols;
+    const cell_height = (max_y - min_y) / rows;
+
+    // Step 3 and 4: Check cells for overlap
+    const polygon = turf.polygon([poly.map(p => [p.x, p.y])]);
+    const rects = [];
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            // Calculate cell bounds
+            const x1 = min_x + col * cell_width;
+            const y1 = min_y + row * cell_height;
+            const x2 = x1 + cell_width;
+            const y2 = y1 + cell_height;
+
+            // Check if cell intersects with polygon
+            const cell_box = turf.bboxPolygon([x1, y1, x2, y2]);
+
+            const intersection = turf.intersect(polygon, cell_box);
+            // Add cell to list of rectangles
+            if (intersection !== null && intersection.geometry.type === 'Polygon' && intersection.geometry.coordinates[0].length > 2) {
+                if (x1 >= min_x && y1 >= min_y && x2 <= max_x && y2 <= max_y) {
+                    rects.push({ x: x1.toFixed(3), y: y1.toFixed(3), width: cell_width.toFixed(3), height: cell_height.toFixed(3) });
+                } else {
+                    // Calculate intersection between cell and polygon
+                    const x3 = Math.max(x1, min_x);
+                    const y3 = Math.max(y1, min_y);
+                    const x4 = Math.min(x2, max_x);
+                    const y4 = Math.min(y2, max_y);
+                    const width = x4 - x3;
+                    const height = y4 - y3;
+                    rects.push({ x: x3.toFixed(3), y: y3.toFixed(3), width: width.toFixed(3), height: height.toFixed(3) });
+                }
+            }
+        }
+    }
+
+    return rects;
+}
 //const floorTexture = new THREE.MeshStandardMaterial({ map: new THREE.TextureLoader().load(marmore) })
 
 function ThreeScene({ cameraStatus, setCamera }) {
@@ -114,14 +168,15 @@ function ThreeScene({ cameraStatus, setCamera }) {
         let vertices = []
         let verticeCount = 0
         let measureVertices = true
-        let angleChange = Math.PI / 180
-        let allSquares = [{x: -0.967, y: -2.9, 'width': 0.644, 'height': 0.644}, {'x': -0.322, 'y': -2.9, 'width': 0.644, 'height': 0.644}, {'x': 0.322, 'y': -2.9, 'width': 0.644, 'height': 0.644}, {'x': -0.967, 'y': -2.256, 'width': 0.644, 'height': 0.644}, {'x': -0.322, 'y': -2.256, 'width': 0.644, 'height': 0.644}, {'x': 0.322, 'y': -2.256, 'width': 0.644, 'height': 0.644}, {'x': -0.967, 'y': -1.611, 'width': 0.644, 'height': 0.644}, {'x': -0.322, 'y': -1.611, 'width': 0.644, 'height': 0.644}, {'x': 0.322, 'y': -1.611, 'width': 0.644, 'height': 0.644}, {'x': -2.9, 'y': -0.967, 'width': 0.644, 'height': 0.644}, {'x': -2.256, 'y': -0.967, 'width': 0.644, 'height': 0.644}, {'x': -1.611, 'y': -0.967, 'width': 0.644, 'height': 0.644}, {'x': -0.967, 'y': -0.967, 'width': 0.644, 'height': 0.644}, {'x': -0.322, 'y': -0.967, 'width': 0.644, 'height': 0.644}, {'x': 0.322, 'y': -0.967, 'width': 0.644, 'height': 0.644}, {'x': 0.967, 'y': -0.967, 'width': 0.644, 'height': 0.644}, {'x': 1.611, 'y': -0.967, 'width': 0.644, 'height': 0.644}, {'x': 2.256, 'y': -0.967, 'width': 0.644, 'height': 0.644}, {'x': -2.9, 'y': -0.322, 'width': 0.644, 'height': 0.644}, {'x': -2.256, 'y': -0.322, 'width': 0.644, 'height': 0.644}, {'x': -1.611, 'y': -0.322, 'width': 0.644, 'height': 0.644}, {'x': -0.967, 'y': -0.322, 'width': 0.644, 'height': 0.644}, {'x': -0.322, 'y': -0.322, 'width': 0.644, 'height': 0.644}, {'x': 0.322, 'y': -0.322, 'width': 0.644, 'height': 0.644}, {'x': 0.967, 'y': -0.322, 'width': 0.644, 'height': 0.644}, {'x': 1.611, 'y': -0.322, 'width': 0.644, 'height': 0.644}, {'x': 2.256, 'y': -0.322, 'width': 0.644, 'height': 0.644}, {'x': -2.9, 'y': 0.322, 'width': 0.644, 'height': 0.644}, {'x': -2.256, 'y': 0.322, 'width': 0.644, 'height': 0.644}, {'x': -1.611, 'y': 0.322, 'width': 0.644, 'height': 0.644}, {'x': -0.967, 'y': 0.322, 'width': 0.644, 'height': 0.644}, {'x': -0.322, 'y': 0.322, 'width': 0.644, 'height': 0.644}, {'x': 0.322, 'y': 0.322, 'width': 0.644, 'height': 0.644}, {'x': 0.967, 'y': 0.322, 'width': 0.644, 'height': 0.644}, {'x': 1.611, 'y': 0.322, 'width': 0.644, 'height': 0.644}, {'x': 2.256, 'y': 0.322, 'width': 0.644, 'height': 0.644}, {'x': -2.9, 'y': 0.967, 'width': 0.644, 'height': 0.644}, {'x': -2.256, 'y': 0.967, 'width': 0.644, 'height': 0.644}, {'x': -1.611, 'y': 0.967, 'width': 0.644, 'height': 0.644}, {'x': -0.967, 'y': 0.967, 'width': 0.644, 'height': 0.644}, {'x': -0.322, 'y': 0.967, 'width': 0.644, 'height': 0.644}, {'x': 0.322, 'y': 0.967, 'width': 0.644, 'height': 0.644}, {'x': 0.967, 'y': 0.967, 'width': 0.644, 'height': 0.644}, {'x': 1.611, 'y': 0.967, 'width': 0.644, 'height': 0.644}, {'x': 2.256, 'y': 0.967, 'width': 0.644, 'height': 0.644}, {'x': -2.256, 'y': 1.611, 'width': 0.644, 'height': 0.644}, {'x': -1.611, 'y': 1.611, 'width': 0.644, 'height': 0.644}, {'x': -0.967, 'y': 1.611, 'width': 0.644, 'height': 0.644}, {'x': -0.322, 'y': 1.611, 'width': 0.644, 'height': 0.644}, {'x': 0.322, 'y': 1.611, 'width': 0.644, 'height': 0.644}, {'x': 0.967, 'y': 1.611, 'width': 0.644, 'height': 0.644}, {'x': 1.611, 'y': 1.611, 'width': 0.644, 'height': 0.644}, {'x': 2.256, 'y': 1.611, 'width': 0.644, 'height': 0.644}, {'x': -1.611, 'y': 2.256, 'width': 0.644, 'height': 0.644}, {'x': -0.967, 'y': 2.256, 'width': 0.644, 'height': 0.644}, {'x': -0.322, 'y': 2.256, 'width': 0.644, 'height': 0.644}, {'x': 0.322, 'y': 2.256, 'width': 0.644, 'height': 0.644}, {'x': 0.967, 'y': 2.256, 'width': 0.644, 'height': 0.644}, {'x': 1.611, 'y': 2.256, 'width': 0.644, 'height': 0.644}, {'x': 2.256, 'y': 2.256, 'width': 0.644, 'height': 0.644}]
+        let angleChange = Math.PI / 90
+        //let allSquares = [{ x: -0.967, y: -2.9, 'width': 0.644, 'height': 0.644 }, { 'x': -0.322, 'y': -2.9, 'width': 0.644, 'height': 0.644 }, { 'x': 0.322, 'y': -2.9, 'width': 0.644, 'height': 0.644 }, { 'x': -0.967, 'y': -2.256, 'width': 0.644, 'height': 0.644 }, { 'x': -0.322, 'y': -2.256, 'width': 0.644, 'height': 0.644 }, { 'x': 0.322, 'y': -2.256, 'width': 0.644, 'height': 0.644 }, { 'x': -0.967, 'y': -1.611, 'width': 0.644, 'height': 0.644 }, { 'x': -0.322, 'y': -1.611, 'width': 0.644, 'height': 0.644 }, { 'x': 0.322, 'y': -1.611, 'width': 0.644, 'height': 0.644 }, { 'x': -2.9, 'y': -0.967, 'width': 0.644, 'height': 0.644 }, { 'x': -2.256, 'y': -0.967, 'width': 0.644, 'height': 0.644 }, { 'x': -1.611, 'y': -0.967, 'width': 0.644, 'height': 0.644 }, { 'x': -0.967, 'y': -0.967, 'width': 0.644, 'height': 0.644 }, { 'x': -0.322, 'y': -0.967, 'width': 0.644, 'height': 0.644 }, { 'x': 0.322, 'y': -0.967, 'width': 0.644, 'height': 0.644 }, { 'x': 0.967, 'y': -0.967, 'width': 0.644, 'height': 0.644 }, { 'x': 1.611, 'y': -0.967, 'width': 0.644, 'height': 0.644 }, { 'x': 2.256, 'y': -0.967, 'width': 0.644, 'height': 0.644 }, { 'x': -2.9, 'y': -0.322, 'width': 0.644, 'height': 0.644 }, { 'x': -2.256, 'y': -0.322, 'width': 0.644, 'height': 0.644 }, { 'x': -1.611, 'y': -0.322, 'width': 0.644, 'height': 0.644 }, { 'x': -0.967, 'y': -0.322, 'width': 0.644, 'height': 0.644 }, { 'x': -0.322, 'y': -0.322, 'width': 0.644, 'height': 0.644 }, { 'x': 0.322, 'y': -0.322, 'width': 0.644, 'height': 0.644 }, { 'x': 0.967, 'y': -0.322, 'width': 0.644, 'height': 0.644 }, { 'x': 1.611, 'y': -0.322, 'width': 0.644, 'height': 0.644 }, { 'x': 2.256, 'y': -0.322, 'width': 0.644, 'height': 0.644 }, { 'x': -2.9, 'y': 0.322, 'width': 0.644, 'height': 0.644 }, { 'x': -2.256, 'y': 0.322, 'width': 0.644, 'height': 0.644 }, { 'x': -1.611, 'y': 0.322, 'width': 0.644, 'height': 0.644 }, { 'x': -0.967, 'y': 0.322, 'width': 0.644, 'height': 0.644 }, { 'x': -0.322, 'y': 0.322, 'width': 0.644, 'height': 0.644 }, { 'x': 0.322, 'y': 0.322, 'width': 0.644, 'height': 0.644 }, { 'x': 0.967, 'y': 0.322, 'width': 0.644, 'height': 0.644 }, { 'x': 1.611, 'y': 0.322, 'width': 0.644, 'height': 0.644 }, { 'x': 2.256, 'y': 0.322, 'width': 0.644, 'height': 0.644 }, { 'x': -2.9, 'y': 0.967, 'width': 0.644, 'height': 0.644 }, { 'x': -2.256, 'y': 0.967, 'width': 0.644, 'height': 0.644 }, { 'x': -1.611, 'y': 0.967, 'width': 0.644, 'height': 0.644 }, { 'x': -0.967, 'y': 0.967, 'width': 0.644, 'height': 0.644 }, { 'x': -0.322, 'y': 0.967, 'width': 0.644, 'height': 0.644 }, { 'x': 0.322, 'y': 0.967, 'width': 0.644, 'height': 0.644 }, { 'x': 0.967, 'y': 0.967, 'width': 0.644, 'height': 0.644 }, { 'x': 1.611, 'y': 0.967, 'width': 0.644, 'height': 0.644 }, { 'x': 2.256, 'y': 0.967, 'width': 0.644, 'height': 0.644 }, { 'x': -2.256, 'y': 1.611, 'width': 0.644, 'height': 0.644 }, { 'x': -1.611, 'y': 1.611, 'width': 0.644, 'height': 0.644 }, { 'x': -0.967, 'y': 1.611, 'width': 0.644, 'height': 0.644 }, { 'x': -0.322, 'y': 1.611, 'width': 0.644, 'height': 0.644 }, { 'x': 0.322, 'y': 1.611, 'width': 0.644, 'height': 0.644 }, { 'x': 0.967, 'y': 1.611, 'width': 0.644, 'height': 0.644 }, { 'x': 1.611, 'y': 1.611, 'width': 0.644, 'height': 0.644 }, { 'x': 2.256, 'y': 1.611, 'width': 0.644, 'height': 0.644 }, { 'x': -1.611, 'y': 2.256, 'width': 0.644, 'height': 0.644 }, { 'x': -0.967, 'y': 2.256, 'width': 0.644, 'height': 0.644 }, { 'x': -0.322, 'y': 2.256, 'width': 0.644, 'height': 0.644 }, { 'x': 0.322, 'y': 2.256, 'width': 0.644, 'height': 0.644 }, { 'x': 0.967, 'y': 2.256, 'width': 0.644, 'height': 0.644 }, { 'x': 1.611, 'y': 2.256, 'width': 0.644, 'height': 0.644 }, { 'x': 2.256, 'y': 2.256, 'width': 0.644, 'height': 0.644 }]
+        /*
         allSquares.forEach((square, index) => {
-            const newSquare = new THREE.Mesh(new THREE.BoxGeometry(square.width*0.975, 0.25, square.height*0.975), new THREE.MeshStandardMaterial({color: '#00ff00'}))
-            newSquare.position.x = square.x+square.width/2 
-            newSquare.position.z = square.y+square.height/2
+            const newSquare = new THREE.Mesh(new THREE.BoxGeometry(square.width * 0.975, 0.25, square.height * 0.975), new THREE.MeshStandardMaterial({ color: '#00ff00' }))
+            newSquare.position.x = square.x + square.width / 2
+            newSquare.position.z = square.y + square.height / 2
             scene.add(newSquare)
-        })
+        })*/
         function animate() {
             requestAnimationFrame(animate);
             camera.rotation.y += angleChange
@@ -133,10 +188,18 @@ function ThreeScene({ cameraStatus, setCamera }) {
                 const currentPoint = { x: intersects[0].point.x, y: intersects[0].point.z }
                 vertices.push(currentPoint)
                 verticeCount++
-                if (verticeCount >= 360) {
+                if (verticeCount >= 1/(angleChange/Math.PI)*2) {
                     measureVertices = false
                     angleChange = 0
-                    let allVertices = ''
+                    vertices.push(vertices[0])
+                    const allSquares = dividePolygon(vertices, 9, 9)
+                    console.log(allSquares.length);
+                    allSquares.forEach((square, index) => {
+                        const newSquare = new THREE.Mesh(new THREE.BoxGeometry(parseFloat(square.width) * 0.975, 0.25, parseFloat(square.height) * 0.975), new THREE.MeshStandardMaterial({ color: '#00ff00' }))
+                        newSquare.position.x = parseFloat(square.x) + parseFloat(square.width) / 2
+                        newSquare.position.z = parseFloat(square.y) + parseFloat(square.height) / 2
+                        scene.add(newSquare)
+                    })
                 }
                 intersects[0].object.material.color.set(0x0000ff)
             }
